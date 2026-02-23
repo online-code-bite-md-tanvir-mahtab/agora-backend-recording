c5759d9714c6186fea7163667d64199eaf910ab0
online-code-bite-md-tanvir-mahtab <tmahtab57@gmail.com>
2026-02-24 05:57:48 +0600

    i have added the sip

--- app.py
+++ app.py
@@ -386,7 +386,7 @@ def inbound_call():
                 "token": TOKEN
             },
             "sipConfig": {
-                "uri": "sip:yourdomain.pstn.ashburn.twilio.com",
+                "uri": "sip:agora736.pstn.ashburn.twilio.com",
                 "username": "tanvir736",
                 "password": "01955005706#@Tan",
                 "callee": from_number
